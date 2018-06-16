85241f7756f7fc417b260ac7cb8694a13f91aaad
Seokseong Jeon <sjeon87@gmail.com>
2018-06-16 10:34:59 +0900

    Add getAsn1ByName

--- parser.js
+++ parser.js
@@ -4,10 +4,24 @@ var path = require('path');
 var bnf = fs.readFileSync(path.resolve(__dirname, 'asn1.jison'), 'utf8');
 var parser = new jison.Parser(bnf);
 
-module.exports = exports = parse;
+module.exports = exports = {
+    parse: parse,
+    getAsn1ByName,
+};
 
 function parse(input) {
     return parser.parse(input);
+}
+
+function getAsn1ByName(name, asn1Json) {
+    let ret = {};
+    for (let moduleName in asn1Json) {
+        let moduleJson = asn1Json[moduleName];
+        if (name in moduleJson) {
+            ret[moduleName] = moduleJson[name];
+        }
+    }
+    return ret;
 }
 
 if (require.main == module) {
