c5cc07f3983b643b8e12ef20dfb42af229f7ecb3
Seokseong Jeon <sjeon87@gmail.com>
2018-10-22 23:09:00 +0900

    Add getUniqueMessageIE

--- parser.js
+++ parser.js
@@ -5,10 +5,9 @@ var path = require('path');
 // var parser = new jison.Parser(bnf);
 var parser = require('./asn1').parser;
 
-module.exports = exports = {
-    parse: parse,
-    getAsn1ByName,
-};
+exports.parse = parse;
+exports.getAsn1ByName = getAsn1ByName;
+exports.getUniqueMessageIE = getUniqueMessageIE;
 
 function parse(input) {
     return parser.parse(input);
@@ -23,6 +22,47 @@ function getAsn1ByName(name, asn1Json) {
         }
     }
     return ret;
+}
+
+function getUniqueMessageIE(messageIEname, asn1Json, moduleName) {
+    if (moduleName) {
+        // 1. Search in the current module
+        if (moduleName in asn1Json &&
+            Object.keys(asn1Json[moduleName]).includes(messageIEname)) {
+                return Object.assign(JSON.parse(JSON.stringify(
+                                        asn1Json[moduleName][messageIEname])),
+                                     {module: moduleName, constants: {}});
+        }
+        // 2. Search in moduleName's import (list of list)
+        for (let importedModuleName in asn1Json[moduleName].import) {
+            let importedModule = asn1Json[moduleName]['import'][importedModuleName];
+            if (importedModule.includes(messageIEname)) {
+                return Object.assign(
+                            JSON.parse(JSON.stringify(
+                                asn1Json[importedModuleName][messageIEname])),
+                            {module: importedModuleName, constants: {}});
+            }
+        }
+    }
+    let messageIEs = getAsn1ByName(messageIEname, asn1Json);
+    let modules = Object.keys(messageIEs);
+    let idx = 0;
+    switch (modules.length) {
+        case 0:
+            throw `No message/IE found`;
+            break;
+        case 1:
+            break;
+        default:
+            console.log(`'${messageIEname}' is defined in multiple modules.`);
+            for (let i = 0; i < modules.length; i++) {
+                console.log(`${i}: ${modules[i]}`);
+            }
+            let idx = readline.questionInt('Which one? ');
+            break;
+    }
+    return Object.assign(JSON.parse(JSON.stringify(messageIEs[modules[idx]])),
+                         {module: modules[idx], constants: {}});
 }
 
 if (require.main == module) {
