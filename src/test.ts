24966834016c889195d712f124a697087a0fb6f2
Seokseong Jeon <sjeon87@gmail.com>
2018-12-19 22:42:30 +0900

    Working on definition interface and its implementations

--- src/test.ts
+++ src/test.ts
@@ -1,7 +1,10 @@
-import {Asn1, Module} from './types';
+import {Asn1, Module, RootIe, Enumerated} from './types';
 
 const asn1: Asn1 = {};
-const eUtraInterndoeDefinitions: Module = new Module('myModule');
+const eUtraInterndoeDefinitions: Module = new Module('module1');
+const rootIe: RootIe = new RootIe('rootIe', new Enumerated());
+
+eUtraInterndoeDefinitions.definitions[rootIe.name] = rootIe;
 asn1[eUtraInterndoeDefinitions.name] = eUtraInterndoeDefinitions;
 
 console.log(asn1);
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
-interface IHash<T> {
+export interface IHash<T> {
     [key: string]: T;
 }
 
@@ -7,11 +7,11 @@ export type Asn1 = IHash<Module>;
 export class Module {
     public name: string;
     public imports: IHash<Import>;
-    public definitions: IHash<Definition>;
+    public definitions: IHash<IDefinition>;
     public constants: IHash<Constant>;
 
     constructor(name: string, imports: IHash<Import> = {},
-                definitions: IHash<Definition> = {},
+                definitions: IHash<RootIe> = {},
                 constants: IHash<Constant> = {}) {
         this.name = name;
         this.imports = imports;
@@ -30,12 +30,37 @@ export class Import {
     }
 }
 
-export class Definition {
+interface IDefinition {
+    Expand(): void;
+}
+
+export class RootIe implements IDefinition {
     public name: string;
-    // TODO
+    public ie: IDefinition;
+
+    constructor(name: string, ie: IDefinition = null) {
+        this.name = name;
+        this.ie = ie;
+    }
+
+    public Expand(): void {
+        return;
+    }
+}
+
+export class Enumerated implements IDefinition {
+    public items: string[];
+
+    constructor(items: string[] = []) {
+        this.items = items;
+    }
+
+    public Expand(): void {
+        return;
+    }
 }
 
 export class Constant {
     public name: string;
-    // TODO
+    public value: number;
 }
